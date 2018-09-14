218b29d1b8daf696e7bbac2c0e3e6a54ebd436c5
cflux1990 <cflux.maltez@live.com>
2018-09-14 07:45:32 -0700

    9.158 - Handling Errors Locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,9 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false,
+
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts') // cant put this in a const variable because it is constantly getting data from server
@@ -24,6 +26,10 @@ class Blog extends Component {
                 this.setState({posts: updatedPosts});
                 // console.log(response);
             })
+            .catch(error => {
+                // console.log(error)
+                this.setState({error: true})
+            })
     }
 
     postSelectedHandler = (id) => {
@@ -33,13 +39,17 @@ class Blog extends Component {
 
 
     render () {
-        const posts = this.state.posts.map(post =>{
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}/>
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+        if (!this.state.error) {
+               posts = this.state.posts.map(post =>{
+                    return <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
         })
+        }
+
         return (
             <div>
                 <section className="Posts">
