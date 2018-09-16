49217c52e9bef806f78706196686fecee383604d
cflux1990 <cflux.maltez@live.com>
2018-09-16 15:00:55 -0700

    11.203 - Loading Routes Lazily

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
-
 
 import './Blog.css';
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+import asyncComponent from '../../hoc/asyncComponent'
+// import NewPost from './NewPost/NewPost'
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
 
 class Blog extends Component {
     state = {
-        auth: false,
+        auth: true,
     }
 
     render () {
@@ -38,7 +41,7 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
                 {/*<Route path="/"  render={() => <h1>Home 2</h1>}/>*/}
                 <Switch> {/*allows react to only render the first one it matches*/}
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} />: null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} />: null}
                     <Route path="/posts" component={Posts}/>
                     <Route render ={() => <h1>Not Found</h1>}/> {/*This is a catch all for all unknown routes. Should always be at end of list. cant work with redirect / */}
                     {/*<Redirect from='/' to='/posts' />*/}
