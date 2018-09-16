3caefe80cf60368c5a1fa94cdbede51b1e04314d
cflux1990 <cflux.maltez@live.com>
2018-09-16 12:51:18 -0700

    11.190 - Passing Route Parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,7 @@ import { Route, NavLink } from 'react-router-dom'
 import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
+import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
 
@@ -37,6 +38,7 @@ class Blog extends Component {
                 {/*<Route path="/"  render={() => <h1>Home 2</h1>}/>*/}
                 <Route path="/" exact component={Posts}/>
                 <Route path="/new-post" component={NewPost}/>
+                <Route path="/:id" exact component={FullPost}/>
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,22 +1,23 @@
 import React, { Component }from 'react';
 import axios from "../../../axios";
+import { Link } from 'react-router-dom';
 
-import Post from '../../../components/Post/Post'
-import './Posts.css'
+import Post from '../../../components/Post/Post';
+import './Posts.css';
 
 
 
 class Posts extends Component {
-       state = {
-            posts: [],
+    state = {
+        posts: [],
 
-            }
+    };
 
     componentDidMount() {
-       console.log(this.props);
+        console.log(this.props);
         axios.get('/posts') // cant put this in a const variable because it is constantly getting data from server
             .then(response => {
-                const posts = response.data.slice(0,4);
+                const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
                     return {
                         ...post,
@@ -30,23 +31,25 @@ class Posts extends Component {
                 console.log(error)
                 // this.setState({error: true})
             })
-        }
+    }
 
     postSelectedHandler = (id) => {
         this.setState({selectedPostId: id});
     }
 
-        render () {
-                    let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+    render() {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
         if (!this.state.error) {
-               posts = this.state.posts.map(post =>{
-                    return <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}/>
-        })
-    }
+            posts = this.state.posts.map(post => {
+                return (
+                    <Link to={'/' + post.id} key={post.id}>
+                        <Post
+                            title={post.title}
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+                    </Link>);
+            });
+
             return (
                 <section className="Posts">
                     {posts} {/*pulls array of posts from post constant*/}
@@ -54,6 +57,7 @@ class Posts extends Component {
 
             )
         }
+    }
 }
 
 export default Posts;
