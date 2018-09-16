d623209ef4bb18eb646daa87b2d6725ba093b02f
cflux1990 <cflux.maltez@live.com>
2018-09-16 14:15:40 -0700

    11.196 - Understanding Nested Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,7 +7,6 @@ import { Route, NavLink, Switch } from 'react-router-dom'
 import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
-import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
 
@@ -19,13 +18,13 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink
-                                to="/"
+                                to="/posts/"
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
                                     textDecoration: 'underline'
-                                }}>Home</NavLink></li>
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash:'#submit',
@@ -36,10 +35,9 @@ class Blog extends Component {
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
                 {/*<Route path="/"  render={() => <h1>Home 2</h1>}/>*/}
-                    <Route path="/" exact component={Posts}/>
                 <Switch> {/*allows react to only render the first one it matches*/}
                     <Route path="/new-post" component={NewPost}/>
-                    <Route path="/:id" exact component={FullPost}/>
+                    <Route path="/posts" component={Posts}/>
                 </Switch>
 
             </div>
