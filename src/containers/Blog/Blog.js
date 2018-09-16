810efe0dc5a94450a7e9ed9f71c8c8c00b99770b
cflux1990 <cflux.maltez@live.com>
2018-09-16 14:43:20 -0700

    11.201 - Working with Guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,9 @@ import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {
-
+    state = {
+        auth: false,
+    }
 
     render () {
         return (
@@ -36,7 +38,7 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
                 {/*<Route path="/"  render={() => <h1>Home 2</h1>}/>*/}
                 <Switch> {/*allows react to only render the first one it matches*/}
-                    <Route path="/new-post" component={NewPost}/>
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} />: null}
                     <Route path="/posts" component={Posts}/>
                     <Redirect from='/' to='/posts' />
                     {/*<Route path="/" component={Posts}/>*/}
