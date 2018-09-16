744fd54f44f379b98726b673fb63600616f47eda
cflux1990 <cflux.maltez@live.com>
2018-09-16 13:10:20 -0700

    11.194 - Navigating Programattically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component }from 'react';
 import axios from "../../../axios";
-import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -33,22 +32,26 @@ class Posts extends Component {
             })
     }
 
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+    postSelectedHandler = ( id ) => {
+        this.props.history.push({pathname: '/' + id});
+        // this.props.history.push({'/' + id})
     }
 
     render() {
         let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
         if (!this.state.error) {
-            posts = this.state.posts.map(post => {
+            posts = this.state.posts.map( post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
+                    // <Link to={'/' + post.id} >
                         <Post
+                            key={post.id}
                             title={post.title}
                             author={post.author}
                             clicked={() => this.postSelectedHandler(post.id)}/>
-                    </Link>);
+                    // </Link>
+                    );
             });
+
 
             return (
                 <section className="Posts">
