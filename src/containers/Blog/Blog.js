d28026996ac537f5a3f5e81767ae9e9eeb692e4d
cflux1990 <cflux.maltez@live.com>
2018-09-16 14:28:50 -0700

    11.198 - Redirecting Requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 
-import { Route, NavLink, Switch } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 
 
 import './Blog.css';
@@ -38,6 +38,9 @@ class Blog extends Component {
                 <Switch> {/*allows react to only render the first one it matches*/}
                     <Route path="/new-post" component={NewPost}/>
                     <Route path="/posts" component={Posts}/>
+                    <Redirect from='/' to='/posts' />
+                    {/*<Route path="/" component={Posts}/>*/}
+
                 </Switch>
 
             </div>
